6b4e2f4318730b8e4314e27d5ed63b19ff635f60
Aline Cristal de Morais <130602346+AlineCristal@users.noreply.github.com>
2024-02-05 18:28:43 -0300

    Add files via upload

---
+++ exercicios-javascript/array.js
@@ -0,0 +1,21 @@
+function IndiceMenorMaior(array) {
+    let maxNum = 0;
+    let minNum = 0;
+
+    for (let i = 1; i < array.length; i++) {
+        if (Number(array[i]) > Number(array[maxNum])) {
+            maxNum = i;
+        }
+        if (Number(array[i]) < Number(array[minNum])) {
+            minNum = i;
+        }
+    }
+
+    return { maxNum, minNum };
+}
+
+const ArrayNumerico = ["7", "100", "58", "85", "41", "19", "23", "74", "11", "3", "152", "65"];
+const indices = IndiceMenorMaior(ArrayNumerico);
+
+console.log("Índice do maior valor:", indices.maxNum);
+console.log("Índice do menor valor:", indices.minNum);
